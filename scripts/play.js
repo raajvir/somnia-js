9a31e3c42f7661de0e0c6d22732ca108346b2580
raajvir <raajvir@icloud.com>
2022-06-20 13:36:00 +0530

    'next'

--- scripts/play.js
+++ scripts/play.js
@@ -2,17 +2,18 @@ function play(type) {
     if (type === 0) {
         console.log('8 minute sleep session initiated')
         location.href = "play.html";
-        initiate(8)
+        initiate(8);
     }
 
     else if (type === 1) {
         console.log('20 minute sleep session initiated')
         location.href = "play.html";
-        initiate(20)
+        initiate(20);
     }
 }
 
 function initiate(time) {
-    var timeSeconds = 60 * time
-    document.getElementById('circle').style.animation="spin2 4s linear infinite";
+    console.log('ejasdf');
+    // var timeSeconds = 60 * time;
+    document.getElementById("circle").style.backgroundColor = "black";
 }
